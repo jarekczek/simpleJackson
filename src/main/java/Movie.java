6397e326638fe012e4186b2053d61139aff03886
Jarek Czekalski <jaroslaw.czekalski@bonsoft.pl>
2017-11-18 13:56:16 +0100

    read and write json data

---
+++ src/main/java/Movie.java
@@ -0,0 +1,30 @@
+public class Movie
+{
+  private String title;
+  private String author;
+  private String director;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getDirector() {
+    return director;
+  }
+
+  public void setDirector(String director) {
+    this.director = director;
+  }
+}
---
+++ src/test/java/Tests.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Tests
+{
+  Logger logger;
+  private File _file;
+
+  public Tests()
+  {
+    logger = Logger.getLogger("bs");
+    _file = new File("plik.json");
+  }
+
+  @Test
+  public void jsonReadWrite() throws IOException {
+    logger.info("This is just a simple test...");
+    Book b = new Book();
+    b.setTitle("W pustyni");
+    ObjectMapper om = new ObjectMapper();
+    om.writeValue(_file, b);
+
+    Book b2 = om.readValue(_file, Book.class);
+    System.out.println("title: " + b2.getTitle());
+    Assert.assertEquals("W pustyni", b2.getTitle());
+
+    Movie m = om.readValue(_file, Movie.class);
+    Assert.assertEquals("W pustyni", m.getTitle());
+  }
+
+  @Test
+  public void jsonReadMoreProps() throws IOException {
+    ObjectMapper om = new ObjectMapper();
+    om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    Movie m = new Movie();
+    m.setTitle("Psy");
+    m.setDirector("Pasikowski");
+    om.writeValue(_file, m);
+    Book b = om.readValue(_file, Book.class);
+    System.out.println("successfully read movie as book");
+    Assert.assertEquals("Psy", b.getTitle());
+  }
+}
